5d74682dae13a91dd9c5546aa897f782977fdf2f
Tylar <murray.tylar@gmail.com>
2025-02-26 13:33:14 -0500

    + unharm noclip

--- scripts/printImageCounts.sh
+++ scripts/printImageCounts.sh
@@ -3,16 +3,18 @@
 # (should be run from PSScene dir)
 
 # Count specific subsets
-harm_clip=$(  ls -1 *_3B_AnalyticMS_SR_8b_harmonized_clip.tif 2>/dev/null | wc -l)
-clip_mask=$(  ls -1 *_3B_udm2_clip.tif                        2>/dev/null | wc -l)
-unharm_clip=$(ls -1 *AnalyticMS_SR_8b_clip.tif                2>/dev/null | wc -l)
-harm_noclip=$(ls -1 *_3B_AnalyticMS_SR_8b_harmonized.tif      2>/dev/null | wc -l)
-noclip_mask=$(ls -1 *_3B_udm2.tif                             2>/dev/null | wc -l)
+harm_clip=$(    ls -1 *_3B_AnalyticMS_SR_8b_harmonized_clip.tif 2>/dev/null | wc -l)
+clip_mask=$(    ls -1 *_3B_udm2_clip.tif                        2>/dev/null | wc -l)
+unharm_clip=$(  ls -1 *AnalyticMS_SR_8b_clip.tif                2>/dev/null | wc -l)
+unharm_noclip=$(ls -1 *SR_8b.tif                                2>/dev/null | wc -l)
+harm_noclip=$(  ls -1 *_3B_AnalyticMS_SR_8b_harmonized.tif      2>/dev/null | wc -l)
+noclip_mask=$(  ls -1 *_3B_udm2.tif                             2>/dev/null | wc -l)
 
 # Display counts for each subset
 echo "Harmonized   noclip 8b    : $harm_noclip"
 echo "Harmonized   clip   8b    : $harm_clip"
 echo "UnHarmonized clip   8b    : $unharm_clip"
+echo "UnHarmonized noclip 8b    : $unharm_noclip"
 echo "             Clip   mask  : $clip_mask"
 echo "             noclip mask  : $noclip_mask"
 
@@ -22,7 +24,7 @@ total=$(ls -1 *tif 2>/dev/null | wc -l)
 echo "Total tif Files         : $total"
 
 # Calculate the accounted for tif files and the remainder
-accounted=$((harm_clip + clip_mask + unharm_clip + harm_noclip + noclip_mask))
+accounted=$((harm_clip + clip_mask + unharm_clip + harm_noclip + noclip_mask + unharm_noclip))
 unaccounted=$((total - accounted))
 
 echo "Unaccounted tif Files   : $unaccounted"
