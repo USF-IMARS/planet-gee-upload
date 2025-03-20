58c2a9077d6afa9e42e4451c2d010ff5aafc1b19
Tylar <murray.tylar@gmail.com>
2025-03-20 14:06:24 -0400

    use gcloud bucket instead of local xml dir

--- scripts/gcloud_to_gee.sh
+++ scripts/gcloud_to_gee.sh
@@ -5,18 +5,18 @@
 #     GEarthEngine including relevant metadata.
 
 # Usage:
-#     ./gcloud_to_gee.sh bucket_prefix dest_collection xml_directory
+#     ./gcloud_to_gee.sh bucket_prefix dest_collection xml_bucket
 
 # Arguments:
-#     - bucket_prefix: name of GCloud bucket
+#     - bucket_prefix: name of GCloud bucket with .tif files
 #     - dest_collection: ImageCollection being created
-#     - xml_directory: filepath to dir with .xml & .json files
+#     - xml_bucket: name of GCloud bucket containing XML and JSON metadata files
 
 # Example usages:
 #     Example 1: Run for St Andrews 8b 
-#       ./gcloud_to_gee.sh planet-st_andrews-8b projects/imars-simm/assets/planet_st_andrews ~/Downloads/stAndrew/StAndrew/PSScene/
+#       ./gcloud_to_gee.sh planet-st_andrews-8b projects/imars-simm/assets/planet_st_andrews planet-xml-metadata-bucket
 #     Example 2: Run for St Andrews masks
-#       ./gcloud_to_gee.sh planet-st_andrews-masks projects/imars-simm/assets/planet_st_andrews_masks ~/Downloads/stAndrew/StAndrew/PSScene/ | tee ../logs/StAndrew_2020_masks_gcloud_to_gee.log 
+#       ./gcloud_to_gee.sh planet-st_andrews-masks projects/imars-simm/assets/planet_st_andrews_masks planet-xml-metadata-bucket | tee ../logs/StAndrew_2020_masks_gcloud_to_gee.log 
 # Dependencies:
 #      https://github.com/7yl4r/filepanther
 #      https://developers.google.com/earth-engine/guides/command_line
@@ -33,13 +33,15 @@ echo_if_test=""  # set this to "echo " to test the script, else set to ""
 
 if [ "$#" -ne 3 ]; then
     echo "Wrong Arguments. Usage: "
-    echo "./gcloud_to_gee.sh <bucket> <dest_collection> <xml_directory>"
+    echo "./gcloud_to_gee.sh <bucket> <dest_collection> <xml_bucket>"
     exit 1
 fi
 
 bucket="$1"
 dest_collection="$2"
-xml_directory="$3"
+xml_bucket="$3"
+
+echo "Will get XML/JSON files from GCloud bucket: $xml_bucket"
 
 echo "Checking if the collection '$dest_collection' exists..."
 # Check if the collection exists and create it if not
@@ -72,21 +74,32 @@ for geotiff in $geotiffs; do
         --pickle_fpath metadata.pickle
 
     echo "*** Estimating xml filename..."
-    # Run filepanther command to format the xml filename pattern and convert it to uppercase
+    # Run filepanther command to format the xml filename pattern
     xml_fileglob=$($filepanther_cmd -q format \
         --pattern "%Y%m%d_%H%M%S_{satellite}_{pass_id}_3B_AnalyticMS_8b_metadata_clip.xml" \
         --pickle_file metadata.pickle)
 
     echo "xml fname is like: ${xml_fileglob}"
 
-    echo "*** Searching for xml file..."
-    # Search for the XML file
-    xml_fpath=$(find "$xml_directory" -name "$xml_fileglob")
+    echo "*** Searching for xml file in GCloud bucket..."
+    # Create a temp directory to store the downloaded XML file
+    temp_dir=$(mktemp -d)
+    
+    # Check if the XML file exists in the bucket
+    gsutil -q ls gs://$xml_bucket/$xml_fileglob > /dev/null
+    if [ $? -eq 0 ]; then
+        echo "Found XML file in GCloud bucket. Downloading..."
+        gsutil cp gs://$xml_bucket/$xml_fileglob $temp_dir/
+        xml_fpath="$temp_dir/$(basename $xml_fileglob)"
+    else
+        echo "XML file not found in GCloud bucket."
+        xml_fpath=""
+    fi
 
     if [ -z "$xml_fpath" ]; then
         echo "xml file not found!"
         # Log the missing XML file
-        echo "missing_xml_file, $filename, find $xml_directory -name $xml_fileglob" >> missing_xml_files.log
+        echo "missing_xml_file, $filename, gsutil ls gs://$xml_bucket/$xml_fileglob" >> missing_xml_files.log
         #exit 1
     else
         echo "Found file: $xml_fpath"
@@ -97,21 +110,29 @@ for geotiff in $geotiffs; do
 	echo "$xml_vars"
 
 	echo "*** Estimating json filename..."
-	# Run filepanther command to format the filename pattern and convert it to uppercase                              
+	# Run filepanther command to format the filename pattern                             
 	json_fileglob=$($filepanther_cmd -q format \
 	    --pattern "%Y%m%d_%H%M%S_{satellite}_{pass_id}_metadata.json" \
 	    --pickle_file metadata.pickle)
 
 	echo "json fname is like: ${json_fileglob}"
 
-	echo "*** Searching for json file..."
-	# Search for the json file  
-	json_fpath=$(find "$xml_directory" -name "$json_fileglob")
+	echo "*** Searching for json file in GCloud bucket..."
+	# Check if the JSON file exists in the bucket
+        gsutil -q ls gs://$xml_bucket/$json_fileglob > /dev/null
+        if [ $? -eq 0 ]; then
+            echo "Found JSON file in GCloud bucket. Downloading..."
+            gsutil cp gs://$xml_bucket/$json_fileglob $temp_dir/
+            json_fpath="$temp_dir/$(basename $json_fileglob)"
+        else
+            echo "JSON file not found in GCloud bucket."
+            json_fpath=""
+        fi
 
 	if [ -z "$json_fpath" ]; then
             echo "json file not found!"
             # Log the missing json file   
-            echo "missing_json_file, $filename, find $xml_directory -name $json_fileglob" >> missing_json_files.log
+            echo "missing_json_file, $filename, gsutil ls gs://$xml_bucket/$json_fileglob" >> missing_json_files.log
             #exit 1
 	else
             echo "Found file: $json_fpath"
@@ -143,5 +164,10 @@ for geotiff in $geotiffs; do
 	    echo "done!"
 	    echo ""
 	fi
+    fi
+    
+    # Clean up temporary directory
+    if [ -n "$temp_dir" ] && [ -d "$temp_dir" ]; then
+        rm -rf "$temp_dir"
     fi
 done
